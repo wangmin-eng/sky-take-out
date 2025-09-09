42afe983ecbd95b5e6f1cc3fa0e7b8eef4d2d4ab
86130 <2867991552@qq.com>
2025-09-09 22:26:42 +0800

    订单支付业务代码开发

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -25,4 +25,12 @@ public interface UserMapper {
      * @param user
      */
     void insert(User user);
+
+    /**
+     * 根据id查询
+     * @param id
+     * @return
+     */
+    @Select("select * from user where id = #{id}")
+    User getById(Long id);
 }
